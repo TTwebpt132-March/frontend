ca964c95858cc9d523c7d95780d4ad4c3c8a6175
Lilia Ovcharenko <lilia.ovcharenko@gmail.com>
2021-03-24 17:48:52 -0700

    worked on login

--- recipes/src/Components/login.js
+++ recipes/src/Components/login.js
@@ -1,11 +1,36 @@
 import React from 'react';
-
 const Login = (props) => {
+ const {values,submit, change} = props
+const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+}
+ const onChange = evt => {
+    const {name, value, type} = evt.target
+    change(name, value)
+}
     return (
-        <div>
-            Login
-        </div>
+        <form className = 'login container' onSubmit = {onSubmit}>
+        <button id = 'loginBtn' disabled={disabled}>Login</button>    
+<div>
+<label>Email&nbsp;
+<input
+value = {values.email}
+onChange = {onChange}
+name = 'email'
+type = 'email'
+/>
+</label>
+<label>Password&nbsp;
+    <input
+    value = {values.password}
+    onChange = {onChange}
+    name = 'password'
+    type = 'password'
+    />
+</label>
+</div>
+        </form>
     )
 }
-
 export default Login;
